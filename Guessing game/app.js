7661fc6c1d2e7e0aebffff6b85111fd59c27af3e
Nazmus Sadat <151758467+Nazmus-Sadat-21@users.noreply.github.com>
2024-10-01 17:12:12 +0600

    Add files via upload

---
+++ Guessing game/app.js
@@ -0,0 +1,31 @@
+const inputbox = document.getElementById("inputbox");
+const btn = document.getElementById("btn");
+const para = document.getElementById("showmssg");
+const para2 = document.getElementById("winloss");
+
+let win =0;
+let loss=0;
+
+btn.addEventListener("click",(e)=>{
+    e.preventDefault();  
+    let randomnum = Math.floor(Math.random()*10)+1;
+    if(inputbox.value==""){
+        alert("Enter a number");
+       
+    }
+    else if(inputbox.value==randomnum){
+        para.innerHTML= `Congratulation,You have won!`;
+        win++;
+        
+    }
+    else{
+        para.innerHTML=`You have lost,random number was: ${randomnum}`;
+        loss++
+    }
+    para2.innerHTML =`Win: ${win} , Loss: ${loss}`;
+    inputbox.value="";
+    
+});
+
+
+
